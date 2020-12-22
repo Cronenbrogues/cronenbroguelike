e9f7b483f33aa2102bfbbb9c706a2603e13f8de5
Cory Massaro <cory.massaro@gmail.com>
2020-12-21 21:39:53 -0800

    Fixes a(n) throughout code base.

--- cronenbroguelike/commands.py
+++ cronenbroguelike/commands.py
@@ -22,14 +22,12 @@ def _look():
         say.insayne(G.player.current_room.description)
 
     # TODO: Bespoke descriptions for all items and characters.
-    # TODO: Fix a(n) problems throughout code base.
     for item in G.player.current_room.items:
         say.insayne(item.idle_description)
     for character in G.player.current_room.npcs:
-        # TODO: Move these descriptions to the actor.
         say.insayne(character.idle_text)
     for corpse in G.player.current_room.corpses:
-        say.insayne(f"The corpse of a(n) {corpse.name} molders here.")
+        say.insayne(f"The corpse of {util.a(corpse.name)} molders here.")
     say.insayne(f'Exits are {", ".join(G.player.current_room.display_exits)}.')
 
 
@@ -186,9 +184,8 @@ def _resolve_attack(attacker, attack):
         # there should be a way to make sure the messages are ordered.
         say.insayne(f"{subj} {hit} {obj} for {damage} damage!")
         # TODO: Attack should have associated text which is consulted here.
-        # TODO: Fix a(n)!
         defender.health.heal_or_harm(
-                -1 * damage, cause=f"the fins of a(n) {attacker.name}")
+                -1 * damage, cause=f"the fins of {util.a(attacker.name)}")
 
 
 def _get_present_actor(actor_name):
@@ -324,7 +321,7 @@ def inspect(item):
     character = G.player.current_room.npcs.find(item_name)
     if character is not None:
         # TODO: Collapse this with descriptive text in look().
-        say.insayne(f"There is a(n) {character.name} slobbering in the corner.")
+        say.insayne(character.idle_text)
         return
 
     say.insayne(f"There is no {item} here to inspect.")
@@ -378,7 +375,7 @@ def use(item, verb):
     item_name = item
     item = G.player.inventory.find(item_name)
     if item is None:
-        say.insayne(f"You don't have a(n) {item_name}.")
+        say.insayne(f"You don't have {util.a(item_name)}.")
         return
     try:
         item.consume(G.player)
@@ -409,7 +406,7 @@ def drop(item):
     item_name = item
     item = G.player.inventory.find(item_name)
     if item is None:
-        say.insayne(f"You don't have a(n) {item_name} to drop.")
+        say.insayne(f"You don't have {util.a(item_name)} to drop.")
     else:
         say.insayne(f"You drop the {item_name} on the ground.")
         _move_item(G.player.inventory, G.player.current_room.items, item)
