677522b47521c0ffe822c9e8c6a421c532d35263
Cory Massaro <cory.massaro@gmail.com>
2020-12-21 20:20:13 -0800

    Restores playability.

--- cronenbroguelike/game.py
+++ cronenbroguelike/game.py
@@ -1,17 +1,4 @@
 import logging
-import random
-import adventurelib
-from . import commands
-from . import floor
-from . import npcs
-from . import rooms
-from engine import actor
-from engine import ai
-from engine import directions
-from engine.globals import G as _G
-from engine.globals import poll_events as _poll_events
-from engine import say
-from engine import when
 
 
 def _load_config():
@@ -27,6 +14,21 @@ def _load_config():
     config.update(additional_config)
     config["log_level"] = config["log_level"].upper()
     return config
+
+
+import random
+import adventurelib
+from . import commands
+from . import floor
+from . import npcs
+from . import rooms
+from engine import actor
+from engine import ai
+from engine import directions
+from engine.globals import G as _G
+from engine.globals import poll_events as _poll_events
+from engine import say
+from engine import when
 
 
 def _create_rooms(number_of_rooms):
@@ -51,51 +53,50 @@ def _get_random_start():
         say.insayne(text)
 
 
-def _start_game(_, CONFIG):
+def _start_game(_, config):
     def startgame(unused_actor):
+        # Creates the player character and ensures game will restart upon death.
+        _G.player = actor.create_actor(
+            health=10,
+            psyche=10,
+            strength=10,
+            stamina=10,
+            will=10,
+            wisdom=10,
+            insanity=0,
+            name="player",
+        )
+        _G.player.log_stats = True
+        _G.player.upon_death(startgame)
+
+        # Creates a small dungeon.
+        level = floor.Floor.generate("cathedral", number_rooms=config["num_rooms"])
+
+        # Places a monster in a random room.
+        level.random_room().add_character(npcs.fish_man())
+
+        # Places an NPC in a random room.
+        level.random_room().add_character(npcs.mad_librarian())
+
+        # Places a cool NPC in a random room.
+        level.random_room().add_character(npcs.smokes_man())
+
+        # Places the player.
+        level.random_room().add_character(_G.player)
+
+        # Starts it up.
+        _get_random_start()
+        adventurelib.set_context("start_game")
+        adventurelib.set_context(None)
         with _poll_events(poll_after=True):
             commands.enter_room(_G.player.current_room)
 
-    # Creates the player character and ensures game will restart upon death.
-    _G.player = actor.create_actor(
-        health=10,
-        psyche=10,
-        strength=10,
-        stamina=10,
-        will=10,
-        wisdom=10,
-        insanity=0,
-        name="player",
-    )
-    _G.player.log_stats = True
-    _G.player.upon_death(startgame)
-
-    # Creates a small dungeon.
-    level = floor.Floor.generate("cathedral", number_rooms=CONFIG["num_rooms"])
-
-    # Places a monster in a random room.
-    level.random_room().add_character(npcs.fish_man())
-
-    # Places an NPC in a random room.
-    level.random_room().add_character(npcs.mad_librarian())
-
-    # Places a cool NPC in a random room.
-    level.random_room().add_character(npcs.smokes_man())
-
-    # Places the player.
-    level.random_room().add_character(_G.player)
-
-    # Starts it up.
-    _get_random_start()
-    adventurelib.set_context("start_game")
     startgame(None)
-    adventurelib.set_context(None)
 
 
 def main():
-    CONFIG = _load_config()
-    logging.basicConfig(level=getattr(logging, CONFIG["log_level"]))
-    _start_game(None, CONFIG)
-    _G.player.upon_death(_start_game)
+    config = _load_config()
+    logging.basicConfig(level=getattr(logging, config["log_level"]))
+    _start_game(None, config)
     adventurelib.say("")  # Necessary for space before first prompt.
     adventurelib.start()
