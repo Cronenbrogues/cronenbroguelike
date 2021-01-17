032a5b2d233363a120b43ea6f1deec5b1536976e
Cory Massaro <cory.massaro@gmail.com>
2021-01-16 19:09:15 -0800

    Cleanup: calls super().setUp() in EngineTest.setUp.

--- tests/common.py
+++ tests/common.py
@@ -6,5 +6,6 @@ from engine.globals import G
 
 class EngineTest(unittest.TestCase):
     def setUp(self):
+        super().setUp()
         # G.player must always exist.
         G.player = actor.create_actor(10, 10, 10, 10, "player")
