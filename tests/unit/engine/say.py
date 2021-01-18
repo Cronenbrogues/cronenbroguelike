83874ff450bbeda302929680bfaeabccf4b0726d
Cory Massaro <cory.massaro@gmail.com>
2021-01-18 13:13:40 -0800

    Adds test for say.

--- tests/unit/engine/say.py
+++ tests/unit/engine/say.py
@@ -1,6 +1,10 @@
+import io
+import re
 import unittest
+from unittest.mock import call
 from unittest.mock import patch
 
+from engine.globals import G
 from engine import say
 from tests import common
 
@@ -19,7 +23,7 @@ def _sifted_edits(original, augmented):
 
     Otherwise, this method will return the longest left substring that can be
     so reconstructed and all edits encountered up to that point. In either case,
-    the returned values will be lists of characters.
+    the returned values will be strings.
 
     This is equivalent to assessing that the edit distance between the two
     strings consists only in a series of additions (not removals or
@@ -44,7 +48,7 @@ def _sifted_edits(original, augmented):
 
     # Captures any remaining characters.
     edits.extend(aug_gen)
-    return reconstructed, edits
+    return ''.join(reconstructed), ''.join(edits)
 
 
 class InsayneTest(common.EngineTest):
@@ -52,14 +56,75 @@ class InsayneTest(common.EngineTest):
     @patch("adventurelib.say")
     def test_add_newline(self, mock_say):
         say.insayne("nasty pervy voices", add_newline=True, insanity=0)
-        mock_say.assert_called_with("")
-        mock_say.assert_called_with("nasty pervy voices")
+        mock_say.assert_has_calls([call.say(""), call.say("nasty pervy voices")], any_order=False)
 
-    def test_sift(self):
-        a, b = _sifted_edits('hello', 'hasdfekldfdloz')
-        self.assertEqual(list('hello'), a)
-        self.assertEqual(list('asdfkdfdz'), b)
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_output_reconstructable(self, mock_stdout):
+        original_text = (
+            "When Gregor Samsa awoke one morning from unsettling dreams, "
+            "he found himself changed in his bed into a monstrous vermin.")
+        say.insayne(original_text, insanity=100)
+        augmented = mock_stdout.getvalue()
+        # adventurelib.say freely introduces newlines.
+        augmented = re.sub(r'\n', r' ', augmented)
+        reconstructed, _ = _sifted_edits(original_text, augmented)
+        self.assertEqual(original_text, reconstructed)
 
-        c, d = _sifted_edits('hello', 'ghoodbyesdfsdf')
-        self.assertEqual(list('he'), c)
-        self.assertEqual(list('goodbysdfsdf'), d)
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_no_alteration_when_insanity_low(self, mock_stdout):
+        original_text = "There is no death in all the world."
+        say.insayne(original_text, insanity=0)
+        augmented = mock_stdout.getvalue()
+        # adventurelib.say freely introduces newlines.
+        augmented = re.sub(r'\n', r' ', augmented)
+        _, edits = _sifted_edits(original_text, augmented)
+        edited_edits = re.sub(r'\s*', r'', edits)
+        self.assertEqual('', edited_edits)
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_sets_zalgochance_when_insanity_high(self, mock_stdout):
+        # TODO: Refactor the zalgotext library to be more testable.
+        original_text = "some text"
+        say.insayne(original_text, insanity=100)
+        augmented = mock_stdout.getvalue()
+        # adventurelib.say freely introduces newlines.
+        augmented = re.sub(r'\n', r' ', augmented)
+        _, edits = _sifted_edits(original_text, augmented)
+        edited_edits = re.sub(r'[A-Z]*', r'', edits)
+        self.assertNotEqual('', edited_edits)
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_sets_zalgochance_when_insanity_high(self, mock_stdout):
+        # TODO: Refactor the zalgotext library to be more testable.
+        original_text = "some text"
+        say.insayne(original_text, insanity=100)
+        augmented = mock_stdout.getvalue()
+        # adventurelib.say freely introduces newlines.
+        augmented = re.sub(r'\n', r' ', augmented)
+        _, edits = _sifted_edits(original_text, augmented)
+        edited_edits = re.sub(r'[A-Z]*', r'', edits)
+        self.assertNotEqual(edits, '')
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_interpolates_voices_when_insanity_high(self, mock_stdout):
+        # TODO: Refactor the zalgotext library to be more testable.
+        original_text = "no, not a text, not a text at all"
+        say.insayne(original_text, insanity=100)
+        augmented = mock_stdout.getvalue()
+        # adventurelib.say freely introduces newlines.
+        augmented = re.sub(r'\n', r' ', augmented)
+        _, edits = _sifted_edits(original_text, augmented)
+        edited_edits = re.sub(r'[^A-Z]*', r'', edits)
+        with open('/tmp/output.txt', 'w') as outp:
+            outp.write(mock_stdout.getvalue())
+        self.assertIsNotNone(re.search(
+                r"^(HEEEHEHEHEHEHE|THEREISNOHOPE|DIDYOUHEARTHAT?"
+                "|IPROMISEYOUKNOWLEDGE)+$", edited_edits))
+
+    @patch("adventurelib.say")
+    @patch("engine.say._hear_voices")
+    def test_player_insanity_used_by_default(self, mock_hear_voices, _):
+        G.player.insanity.modify(20)
+        self.assertEqual(20, G.player.insanity.value)
+        say.insayne("")
+        mock_hear_voices.assert_called_once_with("", G.player.insanity.value)
