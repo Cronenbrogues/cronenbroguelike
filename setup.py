11b58da17cfd12344be4a28a701b8ff7f199e144
Cristina <hi@xmunoz.com>
2021-03-14 18:17:47 -0500

    Upgrade dependency on whimsylib (#133)
    * Upgrade dependency on whimsylib
    
    * Upgrade version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [f"{r.split('/')[-3]} @ {r}" for r in requirements]
 
 setuptools.setup(
     name="cronenbroguelike",
-    version="0.0.4",
+    version="1.0.0",
     author="The Arch Cronenbrogues",
     author_email="cronenbrogues@googlegroups.com",
     description="Â̳ tͥ͞e̅̊x̂ͅtͤ͟-b̝͒à͉s͌͜e̮̓d͚̦-b̨͞o̟ͣdͣyͧ͘-h̛͞o̵ͅr̷͍r̺͡ơ̎r̡̞-r͉̉o͇g̦̏u̦̞e͌͘-l͖̃i̇̈́ḱͅe͢͡ gͧ͜a̜̽mͭ͛e̷͍.",
