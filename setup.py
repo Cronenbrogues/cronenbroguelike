bf69aca1bbccd0a5de6a82bf8914264a9d5133f6
Cristina Muñoz <hi@xmunoz.com>
2021-04-02 18:45:49 -0500

    Create config dir and move game configs there
    - Add global CONFIGS variable to util
    - Load configs from the new location

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fh:
 
 setuptools.setup(
     name="cronenbroguelike",
-    version="2.0.0",
+    version="2.1.0",
     author="The Arch Cronenbrogues",
     author_email="cronenbrogues@googlegroups.com",
     description="Â̳ tͥ͞e̅̊x̂ͅtͤ͟-b̝͒à͉s͌͜e̮̓d͚̦-b̨͞o̟ͣdͣyͧ͘-h̛͞o̵ͅr̷͍r̺͡ơ̎r̡̞-r͉̉o͇g̦̏u̦̞e͌͘-l͖̃i̇̈́ḱͅe͢͡ gͧ͜a̜̽mͭ͛e̷͍.",
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     install_requires=requirements,
     url="https://github.com/Cronenbrogues/cronenbroguelike",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages() + ['config'],
+    include_package_data=True,
     python_requires=">=3.8",
 )
