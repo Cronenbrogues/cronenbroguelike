b7e0b9bd1eefcd6214e4a8df29c96bba298cad01
Cristina Muñoz <hi@xmunoz.com>
2021-04-02 18:52:43 -0500

    Changed my mind about that global var :/

--- cronenbroguelike/util.py
+++ cronenbroguelike/util.py
@@ -5,14 +5,13 @@ import re
 
 from whimsylib.globals import G as _G
 
-CONFIGS = "config"
 
 def read_overridable_config(default_file_name, override_file_name=None):
     if override_file_name is None:
         override_file_name = re.sub(r"\.default(\.json)$", r"\1", default_file_name)
 
     def _load(file_name):
-        with open(os.path.join(CONFIGS, file_name), "r") as inp:
+        with open(os.path.join("config", file_name), "r") as inp:
             return json.load(inp)
 
     config = _load(default_file_name)
