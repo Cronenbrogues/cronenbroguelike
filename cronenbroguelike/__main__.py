ee0d5f5b9b0757cf014587e7235cb05bfb4792ea
Cristina Muñoz <hi@xmunoz.com>
2021-04-06 08:44:15 -0500

    Load configs as resources, eliminate overrideable configs
    This is the only way to get this game to build with pyoxidizer and as a
    python module.
    
    See: https://github.com/indygreg/PyOxidizer/issues/207

--- cronenbroguelike/__main__.py
+++ cronenbroguelike/__main__.py
@@ -4,7 +4,7 @@ import os
 from . import util
 
 
-logging_config = util.read_overridable_config(util.ConfigType.LOGGING)
+logging_config = util.read_config(util.ConfigType.LOGGING)
 logging.basicConfig(level=getattr(logging, logging_config["log_level"].upper()))
 
 
--- cronenbroguelike/util.py
+++ cronenbroguelike/util.py
@@ -2,6 +2,7 @@ import enum
 import json
 import os.path
 import re
+import importlib.resources
 
 
 from whimsylib.globals import G as _G
@@ -13,37 +14,22 @@ class ConfigType(enum.Enum):
 
 
 _CONFIG_MAP = {
-    ConfigType.LOGGING: ["logging_config.default.json", "logging_config.json"],
-    ConfigType.GAME: ["game_config.default.json", "game_config.json"],
+    ConfigType.LOGGING: "logging_config.json",
+    ConfigType.GAME: "game_config.json",
 }
-
-
-_CONFIG_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config")
 
 
 class _ConfigError(Exception):
     pass
 
 
-def read_configs(default_file_name, override_file_name=None):
-    if override_file_name is None:
-        override_file_name = re.sub(r"\.default(\.json)$", r"\1", default_file_name)
-
-    def _load(file_name):
-        with open(file_name, "r") as inp:
-            return json.load(inp)
-
-    config = _load(default_file_name)
-    if os.path.exists(override_file_name):
-        config.update(_load(override_file_name))
-
-    return config
-
-
-def read_overridable_config(config_type):
+def read_config(config_type):
     if config_type not in _CONFIG_MAP:
         raise _ConfigError()
-    default_file_name, override_file_name = [
-        os.path.join(_CONFIG_ROOT, file_name) for file_name in _CONFIG_MAP[config_type]
-    ]
-    return read_configs(default_file_name, override_file_name)
+
+    def _load(file_name):
+        with importlib.resources.open_binary("config", file_name) as fh:
+            data = json.load(fh)
+        return data
+
+    return _load(_CONFIG_MAP[config_type])
